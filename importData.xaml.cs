5a1827cf7586a17876f3eff1238ba244d47fb8a9
Laura Martinez <lauramartinezo182@gmail.com>
2022-01-29 00:18:31 -0500

    Import data button is working

--- importData.xaml.cs
+++ importData.xaml.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,6 +23,10 @@ namespace data_report7
     /// </summary>
     public partial class importData : Window
     {
+        private DataTable info;
+        private ArrayList department;
+
+
         public importData()
         {
             InitializeComponent();
@@ -26,7 +34,15 @@ namespace data_report7
 
         private void btnImportData_Click(object sender, RoutedEventArgs e)
         {
+            department = new ArrayList();
+            var table = new DataTable();
+            OpenFileDialog openFile = new OpenFileDialog();
 
+            if (openFile.ShowDialog() == true)
+            {
+                var path = openFile.OpenFile();
+                using StreamReader reader = new StreamReader(path); 
+            }
         }
 
         private void btnPieChartPage_Click(object sender, RoutedEventArgs e)
